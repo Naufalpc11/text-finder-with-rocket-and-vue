aaa16a64e62a6cd4196dddbd2c07951e58cb185d
Naufalpc11 <tiarana1945@gmail.com>
2025-11-25 10:56:19 +0800

    first commit

---
+++ text-search-ui/src/views/HomePage.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import { pingHello } from "./api";
+
+const backendMessage = ref("Loading...");
+
+onMounted(async () => {
+  try {
+    backendMessage.value = await pingHello();
+  } catch (err) {
+    backendMessage.value = "Error: " + err.message;
+  }
+});
+</script>
+
+<template>
+  <main class="p-4">
+    <h1>Frontend Vue</h1>
+    <p>Respon dari Rocket: {{ backendMessage }}</p>
+  </main>
+</template>
