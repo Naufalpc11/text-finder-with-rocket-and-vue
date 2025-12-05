4bbd8094cd817a2d20072e59de5072b2b1a25ae5
Naufalpc11 <tiarana1945@gmail.com>
2025-12-05 16:39:34 +0800

    make sme fn on constructor

--- text-search-api/src/models/response.rs
+++ text-search-api/src/models/response.rs
@@ -1,38 +1,40 @@
 use serde::Serialize;
 use super::document::DocId;
 
-#[derive(Debug, Clone, Serialize)]
-pub struct PerDocCount {
+macro_rules! derive_response {
+    ($item:item) => {
+        #[derive(Debug, Clone, Serialize)]
+        $item
+    };
+}
+
+derive_response!(pub struct PerDocCount {
     pub doc_id: DocId,
     pub doc_name: String,
     pub count: usize,
     pub snippets: Vec<String>,
-}
+});
 
-#[derive(Debug, Clone, Serialize)]
-pub struct WordResult {
+derive_response!(pub struct WordResult {
     pub word: String,
     pub total_count: usize,
     pub per_doc: Vec<PerDocCount>,
-}
+});
 
-#[derive(Debug, Clone, Serialize)]
-pub struct BenchmarkTiming {
+derive_response!(pub struct BenchmarkTiming {
     pub parallel_ms: f64,
     pub sequential_ms: f64,
     pub speedup: f64,
-}
+});
 
-#[derive(Debug, Clone, Serialize)]
-pub struct DocumentMatch {
+derive_response!(pub struct DocumentMatch {
     pub doc_id: DocId,
     pub doc_name: String,
     pub matched_words: usize,
-}
+});
 
-#[derive(Debug, Clone, Serialize)]
-pub struct SearchResponse {
+derive_response!(pub struct SearchResponse {
     pub results: Vec<WordResult>,
     pub benchmark: BenchmarkTiming,
     pub docs_with_all_words: Vec<DocumentMatch>,
-}
+});
